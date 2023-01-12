d9c8c633292b5ccfb905fd8fe9100a102582e10e
Imamov Ayaz <ayaz.imamov.99@mail.ru>
2023-01-12 21:07:50 +0300

    Add Test with link

---
+++ src/main/java/TestBase.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        List<String> hrefList = new ArrayList();
+
+        int hrefCount = driver.findElements(By.xpath("//li/a[@href]")).size();
+        for (int i = 1; i < hrefCount + 1; i++) {
+            String hrefText = driver.findElement(By.xpath("//li[" + i + "]/a[@href]")).getText();
+            hrefList.add(hrefText);
+            System.out.println(hrefList.get(i - 1));
+        }
+
+        driver.findElement(By.xpath("//li[10]/a[@href]")).click();
+    }
+}
